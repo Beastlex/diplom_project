38c552deed1796d5bddb419b8d69367624fdbc5a
Beastlex <beastlex@gmail.com>
2021-12-07 10:46:12 +0700

    2021-12-07: backend: resources to package. contoller logic for api

---
+++ backend/stat_backend/resources/stats.py
@@ -0,0 +1,26 @@
+from operator import and_
+from flask_restful import Resource
+from models.stats import StatisticsModel
+from datetime import date
+from sqlalchemy import extract, and_
+
+
+class StatisticsList(Resource):
+    def get(self, country, sort_field):
+        today = date.today()
+        if sort_field == "deaths":
+            field = StatisticsModel.deaths
+        else:
+            field = StatisticsModel.date_value
+
+        records = (
+            StatisticsModel.query.filter(
+                and_(
+                    StatisticsModel.country_code == country,
+                    extract("year", StatisticsModel.date_value) == today.year,
+                )
+            )
+            .order_by(field)
+            .all()
+        )
+        return {"statistics": list(map(lambda r: r.json), records)}
