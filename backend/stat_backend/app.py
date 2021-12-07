baefaadb0e86001087bd2ae4a4f8d491f4485fb4
Beastlex <beastlex@gmail.com>
2021-12-07 09:42:27 +0700

    2021-12-07: backend: models to package. refactor app.py

--- backend/stat_backend/app.py
+++ backend/stat_backend/app.py
@@ -8,11 +8,7 @@ from flask_restful import Api
 
 
 def create_app():
-    from stat_backend.api_namespace import api_namespace
-
     application = Flask(__name__)
-    api = Api(application, version="0.1", title="Statistics COVID-19 Backend API",
-              description="Simple API for wrapper for 3d service")
 
     from stat_backend.db import db, db_config
     application.config.update(db_config)
@@ -20,7 +16,6 @@ def create_app():
     migrate = Migrate(application, db)
     application.db = db
 
-    application.add_namespace(api_namespace)
 
     return application
 
