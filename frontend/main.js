a521f694cc934f138a89363a3bef57fc8252da36
beastlex <beastlex@gmail.com>
2021-12-28 02:13:48 +0000

    2021-12-28: frontend: list of countries, perform update

--- frontend/main.js
+++ frontend/main.js
@@ -5,7 +5,7 @@ const formFilter = document.getElementById('filter-form');
 const getLastUpdate = () => {
   const updateMsg = document.getElementById('allready-update-msg');
   try {
-    const response = await fetch('/last_update');
+    const response = await fetch('/api/last_update');
     const result = await response.json();
     updateMsg.textContent = `${result}`;
   } catch (error) {
@@ -14,8 +14,36 @@ const getLastUpdate = () => {
   updateMsg.hidden = false;
 }; 
 
+const getCountryList = () => {
+  const counrtyList = document.getElementById('country-sel');
+  try {
+    const response = await fetch('/api/counties');
+    const result = await response.json();
+    for (country in result) {
+      let newOption = new Option(country, country);
+      counrtyList.append(newOption);
+    }
+  } catch (error) {
+    console.log("Could not load country list. Is it updated?");
+  }
+};
+
+const performUpdate = () => {
+  try {
+    const response = await fetch('/api/update');
+    if (response.status == '200') {
+      getLastUpdate();
+    } else {
+      console.log("Update in progress " + response.status);
+    }
+  } catch (error) {
+    console.log("Update error!")
+  }
+};
+
 formUpdate.addEventListener('submit', (e) => {
   e.preventDefault();
+  performUpdate();
 });
 
 formFilter.addEventListener('submit', (e) => {
@@ -23,3 +51,4 @@ formFilter.addEventListener('submit', (e) => {
 });
 
 getLastUpdate();
+getCountryList();
