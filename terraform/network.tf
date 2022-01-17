da85042272b1bf026a41d5ebd2867eb517996a27
beastlex <beastlex@gmail.com>
2022-01-17 03:12:37 +0000

    2022-01-17: terraform: fix network subnets

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,28 +3,24 @@ resource "azurerm_virtual_network" "vnet-alzver-proj" {
   location  = var.location
   resource_group_name = azurerm_resource_group.rg-alzver-proj.name
   address_space = ["10.0.0.0/16"]
+
+  subnet {
+    name = "subnet-gw"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name = "subnet-db"
+    address_prefix = "10.0.2.0/24"
+  }
+  
+  subnet {
+    name = "subnet-aks"
+    address_prefix = "10.0.3.0/24"
+  }
+  
   tags = {
     owner = var.owner
   }
 }
 
-resource "azurerm_subnet" "subnet-gw" {
-  name = "subnet-gw"
-  resource_group_name = azurerm_resource_group.rg-alzver-proj.name
-  virtual_network_name = azurerm_virtual_network.vne-alzver-proj
-  address_prefix = "10.0.1.0/24"
-}
-
-resource "azurerm_subnet" "subnet-db" {
-  name = "subnet-db"
-  resource_group_name = azurerm_resource_group.rg-alzver-proj.name
-  virtual_network_name = azurerm_virtual_network.vne-alzver-proj
-  address_prefix = "10.0.2.0/24"
-}
-
-resource "azurerm_subnet" "subnet-aks" {
-  name = "subnet-aks"
-  resource_group_name = azurerm_resource_group.rg-alzver-proj.name
-  virtual_network_name = azurerm_virtual_network.vne-alzver-proj
-  address_prefix = "10.0.3.0/24"
-}
