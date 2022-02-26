aeb68cf6ec5260b1395912aa5cf1a3156e53c616
beastlex <beastlex@gmail.com>
2022-02-26 06:35:31 +0000

    2022-02-25: backend: path to MVP 02

---
+++ backend/src/db/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres"
+  }
+}
