aea02a5ac06eb65d91c3ce2f8ae0b1ae03bcbdc5
beastlex <beastlex@gmail.com>
2022-02-28 05:13:40 +0000

    2022-02-28: devel backend: add new model

--- backend/src/db/config/config.js
+++ backend/src/db/config/config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
   "production": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
