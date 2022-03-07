53644bfe7033930173c39997b4934fd20a414b11
beastlex <beastlex@gmail.com>
2022-03-07 05:13:35 +0000

    2022-03-07: backend: MVP add metrics

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const promBundle = require('express-prom-bundle');
 
 const statRouter = require('../routes/stat');
 const updateRouter = require('../routes/update');
 
+const metricsMiddleware = promBundle({
+  autoregister: true,
+  includeStatusCode: true,
+  includePath: true,
+  includeMethod: true,
+});
+
 const app = express();
 app.use(bodyParser.json());
+app.use(metricsMiddleware);
 
 app.use('/api', statRouter);
 app.use('/api', updateRouter);
